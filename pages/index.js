d9eb1872f1cbe0a0608d4832c574633bd5563add
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-10 19:00:40 +0100

    second blog added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -12,45 +12,62 @@ export async function getStaticProps() {
     },
   };
 }
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Welcome to my blog space. Join me on my journey as I take a massive leap into the unknown with a career change from Teacher to Front End Web Developer</p>
         <p>
-          (If you like what you read and would like to know more, please visit{' '}
-          <a className={utilStyles.socialmedialinks} href="https://alanabarrettfrew.com"> my website, or find me on: </a>   <span>
-        <a className={utilStyles.socialmedialinks} href="https://twitter.com/AlanaBF81">
-          <i className="fa fa-twitter"></i>
-        </a>
-        <a className={utilStyles.socialmedialinks} href="https://github.com/AlanaBF">
-          <i className="fa fa-github"></i>
-        </a>
-        <a className={utilStyles.socialmedialinks} href="https://www.linkedin.com/in/alanabarrettfrew">
-          <i className="fa fa-linkedin"></i>
-        </a>
-      </span>.)
+          Welcome to my blog space. Join me on my journey as I take a massive
+          leap into the unknown with a career change from Teacher to Front End
+          Web Developer
         </p>
-      
+        <p>
+          If you like what you read and would like to know more, please visit{" "}
+          <a
+            className={utilStyles.socialmedialinks}
+            href="https://alanabarrettfrew.com"
+          >
+            {""}my website</a>
+             or find me on:
+          <span>{""}
+            <a
+              className={utilStyles.socialmedialinks}
+              href="https://twitter.com/AlanaBF81"
+            >
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a
+              className={utilStyles.socialmedialinks}
+              href="https://github.com/AlanaBF"
+            >
+              <i className="fa fa-github"></i>
+            </a>
+            <a
+              className={utilStyles.socialmedialinks}
+              href="https://www.linkedin.com/in/alanabarrettfrew"
+            >
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </span>
+        </p>
       </section>
       <section className={`${utilStyles.blogcard} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-  <Link href={`/posts/${id}`}>{title}</Link>
-  <br />
-  <small className={utilStyles.lightText}>
-    <Date dateString={date} />
-  </small>
-</li>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
-
     </Layout>
   );
 }
---
+++ posts/always-learning.md
@@ -0,0 +1,40 @@
+---
+title: 'Always Learning!'
+date: '2023-05-10'
+---
+
+Always Learning! I suppose as a teacher I have always tried to share my passion for learning through the lessons I teach. Now as I embark on a career change journey I am actively demonstrating my own passion for learning.
+
+Throughout my career I have always tried to further myself and learn new things. I love it, but I have never come across anything yet that has gripped me as much as Web Development has. I want to do it all the time, genuinely love it, and when I'm not learning new skills through a course, I'm trying to put what I've learnt into practice or coming up with ideas. It's become a bit of an obsession!
+
+The prospect of not having a job is a big worry though. With a mortgage to pay and the tech job market being so overwhelmed at the moment with people like me looking for work, I am trying to upskill whilst also looking at a backup plan.
+
+So what am I currently learning?
+
+Well...
+
+I am currently completing a 4 day course run by *[Kick Off in Business](https://www.kickoffinbusiness.co.uk/)*, which is a funded course.
+
+This will give me two SFEDI Level 1 qualifications:
+- Level 1 Award in Understanding Enterprise
+- Level 1 Extended Award in Passport to Enterprise and Employment
+
+I am looking at the option of being a freelance Web Developer and Maths Tutor whilst I look for a permanent job.
+
+I am also continuing to develop my Web Development Knowledge and so I am working my way through an Udemy Course: The Complete 2023 Web Development Bootcamp by Dr Angela Yu
+
+- *Become a Full-Stack Web Developer with just ONE course.*
+
+- *HTML, CSS, Javascript, Node, React, MongoDB, Web3 and DApps*
+
+I am particularly enjoying learning more about the backend side of coding with Node.js, Express and most recently about templating with EJS. There are so many different ways to achieve a similar result. It's a fascinating new world...
+
+With learning new and complex content one thing I have learnt is that repetition is key so I try where I can in all the excitement of wanting to learn as much as I can to revise content by a process of space repetition:
+
+- This involves learning the content
+- Revise it within 24 hours
+- Then revise again 3 days later
+- Then revise again 7 days later
+- Gradually increasing the time between each revision until it is committed to long term memory.
+
+I'm also learning that there is so much to learn that you don't have to remember it all as that is what the documentation is for... and Google!
